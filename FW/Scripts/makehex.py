1170f10952b43e1616c4e0428a05c9bf1f63b9e0
Valentina Rudaljevic <valentinarudaljevic01@gmail.com>
2023-06-15 09:40:25 +0200

    Modified makehex.py

--- FW/Scripts/makehex.py
+++ FW/Scripts/makehex.py
@@ -11,20 +11,18 @@ from sys import argv
 
 binfile = argv[1]
 nwords = int(argv[2])
+hexfile = argv[3]  # New argument for the output .hex file
 
 with open(binfile, "rb") as f:
     bindata = f.read()
 
-#print("len(bindata)", len(bindata))
+with open(hexfile, "w") as f:  # Open the output .hex file in write mode
+    for i in range(nwords):
+        if i < len(bindata) // 4:
+            w = bindata[4*i : 4*i+4]
+            hex_str = "%02x%02x%02x%02x" % (w[3], w[2], w[1], w[0])
+        else:
+            hex_str = "0"
+        f.write(hex_str + "\n")  # Write the hex string to the .hex file
 
-
-#assert len(bindata) < 4*nwords
-#assert len(bindata) % 4 == 0
-
-for i in range(nwords):
-    if i < len(bindata) // 4:
-        w = bindata[4*i : 4*i+4]
-        print("%02x%02x%02x%02x" % (w[3], w[2], w[1], w[0]))
-    else:
-        print("0")
 
