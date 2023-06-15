01eac08589128e948fc67ff63a7e32267b76bbd5
Valentina Rudaljevic <valentinarudaljevic01@gmail.com>
2023-06-15 14:37:33 +0200

    Added flow.sh script

---
+++ Arduino_max1000_package/flow.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+mkdir /home/.arduino15/packages/max1000/tools
+
+# Nakon klonovanja foldera sa GitHub-a, na putanji custom_folder napraviti novi folder sa nazivom tools i u okviru njega instalirati .zip sa linka ispod
+
+#https://drive.google.com/file/d/1Hg5zzNHYCGyKE_16rAQBer1kZUNvDtXI/view?usp=drive_link
+
+
+exit 0
